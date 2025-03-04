1fe64cab1f8771723b32fe618658e4cb358b55b6
Payden Hoskins <paydenhoskins@gmail.com>
2025-03-04 08:23:15 -0700

    Added a U and L Case code

--- WinFormExample/WinFormExampleForm.vb
+++ WinFormExample/WinFormExampleForm.vb
@@ -16,7 +16,14 @@ Public Class WinFormExampleForm
     End Sub
 
     Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
-        Me.Text = StrReverse(Me.Text)
+        'Me.Text = StrReverse(Me.Text)
+        If UpperRadioButton.Checked Then
+            Me.Text = UCase(ThirdTextBox.Text & " " & SecondTextBox.Text)
+        ElseIf UpperRadioButton.Checked = False Then
+            Me.Text = LCase(ThirdTextBox.Text & " " & SecondTextBox.Text)
+        Else
+            MsgBox($"Oh No! Nothing Selected!")
+        End If
     End Sub
 
     Private Sub ChangeButton_Click(sender As Object, e As EventArgs) Handles ChangeButton.Click
